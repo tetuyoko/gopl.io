05d51b0f8682616875d14d0763837b57671f907f
tetuyoko <tyokoyama53@gmail.com>
2016-06-19 17:04:05 +0900

    GifServer

--- ch1/server2.go
+++ ch1/server2.go
@@ -5,12 +5,30 @@ import (
 	"log"
 	"net/http"
 	"sync"
+
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	_ "os"
+	"time"
 )
 
 var mu sync.Mutex
 var count int
 
+var palette = []color.Color{color.Black, color.White, color.RGBA{255, 0, 0, 1}}
+
+const (
+	blackIndex = 0
+	whiteIndex = 1
+	redIndex   = 2
+)
+
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
 	http.HandleFunc("/count/", counter)
@@ -21,7 +39,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
 	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	//fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 	lissagejous(w)
 }
 
@@ -29,4 +47,31 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
+}
+
+func lissagejous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			//img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), redIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(rand.Intn(3)))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
 }
